f37837029f2b7facdc49b514346b31fe9218d8c0
Izhaki <roey@gefri.org>
2023-03-06 12:44:15 +0000

    Use esm module resolution

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    fullySpecified: true,
+  },
+  // See https://github.com/vercel/next.js/issues/41961
+  // This solves the issue, but should be part of `fullySpecified`.
+  // Also, this is Webpack only, not SWC solution.
+  //
+  // webpack: (webpackConfig) => {
+  //   webpackConfig.resolve.extensionAlias = {
+  //     ".js": [".ts", ".tsx", ".js", ".jsx"],
+  //     ".mjs": [".mts", ".mjs"],
+  //     ".cjs": [".cts", ".cjs"],
+  //   };
+  //   return webpackConfig;
+  // },
+};
 
-module.exports = nextConfig
+export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import test from '@/test.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  test()
   return (
     <>
       <Head>
